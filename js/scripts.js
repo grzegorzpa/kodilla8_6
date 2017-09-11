637baa07a19ff86ef674769527bd96648a812b4e
Grzegorz Pawlak <kalejdoskopwroclaw@gmail.com>
2017-09-11 18:20:37 +0200

    add

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,17 @@
 // scripts.js
-var	a = prompt('Dłguść boku:','10')
-	h = prompt('Podaj wysokość:', '12')
-	triangleArea = a * h /2
-document.getElementById("prompt").innerHTML =
-"Pole Twojego Trójkąta wynosi: " + triangleArea;
-alert('Twój wynik to ' + triangleArea);
-console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
+var	a = prompt('Wartość A:','6')
+	b = prompt('Wartość B:', '7')
+	value = (a * a) + (2 * a * b) - (b * b)
+console.log('Wynik działania ' + a + ' and ' + b + ' is equal to ' + value)
+
+if (value > 0) {
+	alert('Liczba jest dodatnia')
+	console.log('Liczba jest większa niż 0');
+} else if (value < 0) {
+	alert('Liczba jest ujemna')
+	console.log('Liczba jest mniejsza niz 0');
+} else {
+	alert('Liczba równa się 0')
+	console.log('Wynik równa się 0');
+}
+document.getElementById("prompt").innerHTML = 'Twój wynik to:' + value
